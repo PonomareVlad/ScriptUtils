79ca14ef7e141f519adfe61ec22382e05ca2d120
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-02-23 22:59:15 +0500

    Inject with proper headers

--- api/inject.mjs
+++ api/inject.mjs
@@ -1,6 +1,8 @@
 import {serializeError} from "serialize-error";
 
-export default async ({query}, {send}) => {
+export default async ({query}, res) => {
+    res.setHeader(`Content-Disposition`, `inline; filename="script.js"`)
+    res.setHeader(`Content-Type`, `application/javascript; charset=utf-8`)
     try {
         const {source, inject, position} = query
         const scripts = await Promise.all([
@@ -8,8 +10,8 @@ export default async ({query}, {send}) => {
             fetch(inject).then(response => response.text()),
         ])
         const result = position === "before" ? scripts.reverse() : scripts
-        return send(result.join(`\r\n`))
+        return res.send(result.join(`\r\n`))
     } catch (error) {
-        return send(`throw Error(${JSON.stringify(serializeError(error))})`)
+        return res.send(`throw Error(${JSON.stringify(serializeError(error))})`)
     }
 }
